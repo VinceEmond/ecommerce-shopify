b2b7516c3c74a9f231eae045a1b2cdafdf52f283
Vince Emond <vince.emond@icloud.com>
2022-06-15 13:42:12 -0700

    Narrowing

--- playground.ts
+++ playground.ts
@@ -1,53 +1,14 @@
 
-
-// interface Person {
-//   name: string
-//   age: number
-// }
-
-class Person {
-  name: string
-  age?: number
-
-  constructor(name: string, age: number){
-    this.name = name
-    this.age = age
-  }
-}
-
-type PersonLoggerFn = (name: string, age?: number) => string
-
 export default function play() {
-  const name: string = "Vince"
-  const age: number = 30
-  const person = {
-    name: 'John',
-    age: 34
-  } 
 
-  const logPersonInfo: PersonLoggerFn = (
-    personName: string, personAge: number = 0
-  ): string => {
-    const info = `Name: ${personName}, age: ${personAge}`
-    console.log(info)
-    return info
-  }
+  const random = Math.random() > 0.5 ? "hello" : [1,2]
 
-  function logPersonInfo2(person: Person) {
-    const info = `Name: ${person.name}, age: ${person.age}`
-    console.log(info)
-    return info
+  if (typeof random === "string"){
+    const upper = random.toUpperCase();
   }
   
-  const log = logPersonInfo(name)
-
-  const aPerson = new Person("Jimmy", 69)
-  const john: Person = {
-    name: "john"
-  }
-
-  // const log: string = logPersonInfo(name,age)
-  logPersonInfo2(aPerson)
+  console.log(random)
+  console.log(random.length);
 }
 
 
