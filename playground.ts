c5605462dbd0ccf91fe2dafa90eb42be7073f2dc
Vince Emond <vince.emond@icloud.com>
2022-06-15 14:33:40 -0700

    Union Narrowing

--- playground.ts
+++ playground.ts
@@ -13,19 +13,30 @@ interface AcademicPerson extends Person{
 
 type RaceCar = {
   name: string
-  maxSpeed: number
+  maxSpeed: 200
+  team: string
 }
 type CityCar = {
   name: string
   space: string
+  maxSpeed: 100
 }
 
 type Car = CityCar & RaceCar
 
 export default function play() {
+
+  const car: RaceCar = {
+    name: "race car",
+    maxSpeed: 200,
+    team: "superteam"
+  }
+
   function logCarInfo(car: Car){
-    console.log(car.name)
-    console.log(car.maxSpeed)
+    // console.log(car.name)
+    // console.log(car.maxSpeed)
+
+    switch(car.maxSpeed)
   }
 }
 
