08d667d3a487ecb5fd73b107757331db41f0daa7
Vince Emond <vince.emond@icloud.com>
2022-06-15 13:21:44 -0700

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react"
+import play from "../playground";
+
+export default function Home() {
+
+useEffect(()=> {
+  play();
+}, [])
+
+  return (
+    <div>
+        Hello World
+    </div>
+    )
+}
+ 
+
+
+ 
