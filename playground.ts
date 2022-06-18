2d3edd3cda50fb13b7dbee7d584c881b9d2f31d2
Vince Emond <vince.emond@icloud.com>
2022-06-17 21:34:46 -0700

    Array index types

--- playground.ts
+++ playground.ts
@@ -1,41 +1,17 @@
+
 interface Person {
   name: string
 }
 
-interface Student extends Person {
-  age: number
+type CustomArray = {
+  [index: number]: string
 }
 
-interface PostGraduateStudent extends Person {
-  age: number
-  projects: string[]
-}
-
-// interface StudentInfo<T extends Student = Student> {
-//   data: T
-//   grades: number[]
-// }
-
-type StudentInfo<T extends Student = Student> = {
-  data: T
-  grades: number[]
-}
 
 export default function play() {
 
-  function logStudentInfo(info: StudentInfo) {
-    console.log(info.data.name)
-    console.log(info.data.age)
-  }
+  const items: CustomArray = ['1','2','3']
 
-  const info = {
-    data: {
-      name: "Vince",
-      age: 65
-    },
-      grades: [12,32,43]
-    }
-    logStudentInfo(info)
 }
 
 
