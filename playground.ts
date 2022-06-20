ecd2b50ecd103de37cb2704a6b763026f41917fb
Vince Emond <vince.emond@icloud.com>
2022-06-20 13:06:28 -0700

    Multiple generic parameters

--- playground.ts
+++ playground.ts
@@ -4,14 +4,24 @@ interface Person {
   age: number
 }
 
-type PersonKey = keyof Person
-
+type Logger<
+  P1 = string,
+  P2 = number,
+  RT = string
+  > = (param1: P1, param2: P2) => RT
 
 export default function play() {
 
+// Uses custom values
+const superLogger: Logger<string, number, string> = (name, age) => {
+  return "Hello World!"
+} 
+
+// Uses the default values
+const logger: Logger = (brand, age) => {
+  return 'car'
+}
   
-  const personKey: PersonKey = 'name'
-  const personKey2: PersonKey = 'age'
 }
  
 
