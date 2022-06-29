88589b3648aab60b73ae8ca24f00fc4328e50074
Vince Emond <vince.emond@icloud.com>
2022-06-29 12:23:24 -0700

    Added comments for clarity

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -9,10 +9,13 @@ type ReturnType = {
 const getAllProducts = async (): Promise<any> => {
   const { data } = await fetchApi<ReturnType>({query: getAllProductsQuery})
 
-  // Normalize and return new data!
+  // Normalize and return new data
+  // --> Destructuring edge to node, then using alias "product"
+  // --> ?? at the end is to catch undefined/null and return empty array
   const products = data.products.edges.map(({ node: product }) => {
     return product
   }) ?? []
+
 
   return products
 }
