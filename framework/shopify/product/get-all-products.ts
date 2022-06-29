d14b5e270ff0c164f2a43fea49258bca529d24e9
Vince Emond <vince.emond@icloud.com>
2022-06-29 11:11:53 -0700

    Implemented fetch product query to pull from fake shopify server

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,14 +1,22 @@
 
+import getAllProductsQuery from "../utils/queries/get-all-products";
 
-const fetchApi = async () => {
-  const url = 'https://jsonplaceholder.typicode.com/todos'
+type FetchParams = {
+  query: string
+}
 
-  // Make GET request for JSON data 
+const fetchApi = async ({query}: FetchParams) => {
+  const url = 'http://localhost:4000/graphql'
+
+  // Make POST request for JSON data using query
   const res = await fetch(url, {
-    method: 'GET',
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-    }
+    },
+    body: JSON.stringify({
+      query: getAllProductsQuery
+    })
   })
 
   const data = await res.json();
@@ -18,7 +26,7 @@ const fetchApi = async () => {
 
 
 const getAllProducts = async (): Promise<any[]> => {
-  const products = await fetchApi()
+  const products = await fetchApi({query: getAllProductsQuery})
   return products.data
 }
 
