5660e35a88388b0e5d46ddabacaab95785683b7b
Vince Emond <vince.emond@icloud.com>
2022-07-15 18:01:12 -0700

    TS Config Specific Setup - Allows switching between shopify/bigcommerce

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,7 +1,9 @@
 const path = require("path");
+const fs = require("fs");
 const merge = require("deepmerge");
 
 function withFramworkConfig(defaultConfig = {}) {
+  // Change this line to "shopify" or "bigcommerce" respectively
   const framework = "shopify";
 
   const frameworkNextConfig = require(path.join(
@@ -9,8 +11,15 @@ function withFramworkConfig(defaultConfig = {}) {
     framework,
     "next.config"
   ));
-
   const config = merge(defaultConfig, frameworkNextConfig);
+
+  const tsPath = path.join(process.cwd(), "tsconfig.json");
+  const tsConfig = require(tsPath);
+
+  tsConfig.compilerOptions.paths["@framework"] = [`framework/${framework}`];
+  tsConfig.compilerOptions.paths["@framework/*"] = [`framework/${framework}/*`];
+
+  fs.writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2));
 
   return config;
 }
