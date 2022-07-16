8a0e48c5e0aab3ad0802757aeb97a066272fdfec
Vince Emond <vince.emond@icloud.com>
2022-07-15 17:44:06 -0700

    Setup Framework Specific next.config fof shopify/bigcommerce and merge them in config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const { withFramworkConfig } = require("./framework/common/config");
+
+module.exports = withFramworkConfig({
   reactStrictMode: false,
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US",
   },
-};
+});
 
 console.log("nextconfig.js", JSON.stringify(module.exports, null, 2));
