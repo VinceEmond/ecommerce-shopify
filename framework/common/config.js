62f44a6103230810067bef38daa16adaae287400
Vince Emond <vince.emond@icloud.com>
2022-07-16 13:52:25 -0700

    Handle corner cases for shopify vs bigcommerce config loading

--- framework/common/config.js
+++ framework/common/config.js
@@ -3,10 +3,31 @@ const fs = require("fs");
 const merge = require("deepmerge");
 const prettier = require("prettier");
 
+const ALLOWED_FRAMEWORKS = ["shopify", "bigcommerce", "shopify_local"];
+const FALLBACK_FRAMEWORK = "shopify";
+
 function withFrameworkConfig(defaultConfig = {}) {
   // Function to switch which config file to load + merge
 
-  const framework = defaultConfig?.framework.name; // Default config is pulled from next.config.js
+  let framework = defaultConfig?.framework?.name; // Default config is pulled from next.config.js
+
+  if (!framework) {
+    throw new Error(
+      "The api framework is missing, please add a valid provider!"
+    );
+  }
+
+  if (!ALLOWED_FRAMEWORKS.includes(framework)) {
+    throw new Error(
+      `The api framework "${framework}" cannot be found. Please use one of the following options: ${ALLOWED_FRAMEWORKS.join(
+        ", "
+      )}`
+    );
+  }
+
+  if (framework === "shopify_local") {
+    framework = FALLBACK_FRAMEWORK;
+  }
 
   const frameworkNextConfig = require(path.join(
     "../",
