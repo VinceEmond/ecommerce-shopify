20494f411aedca4ef5fdf894fb788b6fa67e7b86
Vince Emond <vince.emond@icloud.com>
2022-08-03 17:49:16 -0700

    Added NextJS link to product card along with hardcoded price as well as a placeholder product image

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,18 +1,38 @@
 import { Product } from "@common/types/product";
 import { FC } from "react"
+import Link  from "next/link"
+import Image from "next/image"
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 
 interface Props {
   product: Product
 }
 
+const placeholderImage = "/product-image-placeholder.svg"
+
 const ProductCard: FC<Props> = ({product}) => {
-
-
-
   return (
-    <div>
-      {product.name}
-    </div>
+    <Link href={`/products/${product.slug}`}>
+      <a> 
+        <div>
+          <h3>
+            <span>{product.name}</span>
+          </h3>
+          <span>$14</span>
+        </div>
+        { product.images && (
+          <Image 
+            alt={product.name ?? "Product Image"}
+            src={placeholderImage}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+            />
+          )
+        }
+      </a>
+    </Link>
   )
 }
 
